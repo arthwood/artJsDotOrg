007ff92918a18e02e251f495113e06c2b4e6342a
arthwood <arthwood@llp.pl>
2010-06-27 13:02:33 +0200

    ie fixes

--- javascripts/art.js
+++ javascripts/art.js
@@ -3,7 +3,7 @@ var ArtJs_version = {};
 ArtJs_version['Microsoft Internet Explorer'] = 'ie';
 ArtJs_version['Netscape'] = 'ff';
 
-var ArtJs_package = ArtJs_version[window.navigator.appName];
-var ArtJs_path = window.ArtJs_path || 'javascripts/';
+var ArtJs_package = ArtJs_version[navigator.appName];
+var ArtJs_path = ArtJs_path || 'javascripts/';
 
 document.write('<script src="' + ArtJs_path + 'art.' + ArtJs_package + '.js" type="text/javascript"></script>');
